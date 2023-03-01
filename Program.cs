1fe43e502046ef6791d3ae7850cf58a4c4a2dbd4
hp <hp@HPELITEBOOK840G>
2023-03-01 12:29:19 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿class xyz
+{
+    public static void Main(String[] args)
+    {
+        int i = 1;
+        string xyz = "https:\\www.softgridinfo.in";
+
+        while (i <= 100000)
+        {
+            Console.WriteLine(xyz);
+            i++;
+        }
+
+    }
+}
